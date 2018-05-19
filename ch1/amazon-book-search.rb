d158b3c18d421ebbed839a4bb46d43303a6e7bff
jellyfish77 <>
2018-05-19 10:19:36 +0800

    add ch1

---
+++ ch1/amazon-book-search.rb
@@ -0,0 +1,16 @@
+#Example 1-2. Searching for books with a Ruby script
+#!/usr/bin/ruby -w
+# amazon-book-search.rb
+require 'amazon/search'
+
+if ARGV.size != 2
+	puts "Usage: #{$0} [Amazon Web Services AccessKey ID] [text to search for]"
+	exit
+end
+access_key, search_request = ARGV
+req = Amazon::Search::Request.new(access_key)
+# For every book in the search results...
+req.keyword_search(search_request, 'books', Amazon::Search::LIGHT) do |book|
+	# Print the book's name and the list of authors.
+	puts %{"#{book.product_name}" by #{book.authors.join(', ')}}
+end
